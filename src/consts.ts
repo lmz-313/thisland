41f633ca70f3b0e02722e31272f1f98f3aa0d30a
LongMenJuZhen <cyc313@outlook.com>
2025-06-18 02:09:17 +0800

    存一下

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_NAME = '此地';
+export const SITE_SLOGAN = '互联网的一座岛屿';
